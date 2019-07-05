6de9f7b4a1dbc3f94eca28012f2d207835f45bcf
wangjun <wang.jun16@163.com>
2019-07-05 16:29:12 +0800

    删除无用的文件

--- lib/page/movie/movie_page.dart
+++
@@ -1,9 +0,0 @@
-import 'package:flutter/material.dart';
-
-class MoviePage extends StatefulWidget {
-  @override
-  State<StatefulWidget> createState() {
-    // TODO: implement createState
-    return null;
-  }
-}
